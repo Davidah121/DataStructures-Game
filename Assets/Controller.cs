9091d29a6d269a32330e0cda13080c89964936de
Davidah121 <42045018+Davidah121@users.noreply.github.com>
2019-04-02 17:20:09 -0400

    Initial commit

---
+++ Assets/Controller.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public static Controller c;
+
+    public Stack<int> gameStack = new Stack<int>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameStack.Clear();
+    }
+
+    void push(int value)
+    {
+        gameStack.Push(value);
+    }
+
+    int getSize()
+    {
+        return gameStack.Count;
+    }
+
+    int pop()
+    {
+        return gameStack.Pop();
+    }
+
+    int peek()
+    {
+        return gameStack.Peek();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody2D rb2d;
+    [SerializeField] private float speed = 1;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 MoveForce = new Vector2(0, 0);
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            MoveForce.x = -1;
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            MoveForce.x = 1;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            MoveForce.y = 1;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            MoveForce.y = -1;
+        }
+
+        rb2d.velocity = MoveForce;
+        
+    }
+}
