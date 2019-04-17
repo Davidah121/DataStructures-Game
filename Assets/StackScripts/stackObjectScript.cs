1539611187cf9a40a2bac42efa22bf5fd88be4f2
Davidah121 <42045018+Davidah121@users.noreply.github.com>
2019-04-16 21:47:30 -0400

    Almost Finished the stack game
    Added particle effects, Music, random sound effects that play based on the current key of the song (What chord you would play over that particular section). The only issue is that the song continues to play after going back to the main menu. Sound effects are custom made.

--- Assets/StackScripts/stackObjectScript.cs
+++ Assets/StackScripts/stackObjectScript.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class stackObjectScript : MonoBehaviour
 {
     [SerializeField] int id = 0; //Not a valid number
-    
+    [SerializeField] GameObject partObject;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -34,6 +35,64 @@ public class stackObjectScript : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        
+
+        int tSamples = (int)(GameObject.Find("BOTW_Music").GetComponent<AudioSource>().time/10.5f);
+
+        if (tSamples%2 == 0)
+        {
+            //Assume Major Section
+            int tVal = Random.Range(1, 4);
+            switch (tVal)
+            {
+                case 1:
+                    GameObject.Find("A2").GetComponent<AudioSource>().Play();
+                    break;
+                case 2:
+                    GameObject.Find("A3").GetComponent<AudioSource>().Play();
+                    break;
+                case 3:
+                    GameObject.Find("A5").GetComponent<AudioSource>().Play();
+                    break;
+                case 4:
+                    GameObject.Find("A8").GetComponent<AudioSource>().Play();
+                    break;
+                default:
+                    break;
+            }
+        }
+        else
+        {
+            //Assume Minor Section
+            int tVal = Random.Range(1, 5);
+            switch (tVal)
+            {
+                case 1:
+                    GameObject.Find("A1").GetComponent<AudioSource>().Play();
+                    break;
+                case 2:
+                    GameObject.Find("A3").GetComponent<AudioSource>().Play();
+                    break;
+                case 3:
+                    GameObject.Find("A5").GetComponent<AudioSource>().Play();
+                    break;
+                case 4:
+                    GameObject.Find("A6").GetComponent<AudioSource>().Play();
+                    break;
+                case 5:
+                    GameObject.Find("A7").GetComponent<AudioSource>().Play();
+                    break;
+                default:
+                    break;
+            }
+
+        }
+        Vector3 tempVec = this.transform.position;
+        tempVec.z = -1;
+        Quaternion q = Quaternion.Euler(90, 0, 0);
+        
+        GameObject.Instantiate(partObject, tempVec, q);
+        
         Controller.c.push(id);
         Destroy(this.gameObject);
     }
