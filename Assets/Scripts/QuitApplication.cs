1ae877e51356246899e07fe8192c947948b5134d
thodzen <thodzen@gmail.com>
2019-04-18 15:34:44 -0400

    Added Working Quit Buttons

---
+++ Assets/Scripts/QuitApplication.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitApplication : MonoBehaviour
+{
+   public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Application Quit");
+    }
+}
